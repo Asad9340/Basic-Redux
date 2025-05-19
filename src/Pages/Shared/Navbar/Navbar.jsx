a8cefaba1e76956021d043b32310b7c25658e97e
Asad9340 <asadulimran1999@gmail.com>
2025-05-20 01:04:49 +0600

    redux setup complete

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <div>Logo</div>
+      <div>Link</div>
+      <div>Profile</div>
+    </div>
+  )
+}
+
+export default Navbar
