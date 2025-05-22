a02d8cfb4870b85682c8df89349cf15ac31df390
Asad9340 <asadulimran1999@gmail.com>
2025-05-22 14:26:52 +0600

    design modify

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,15 +1,16 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Pages/Shared/Navbar/Navbar"
-import Footer from "../Pages/Shared/Footer/Footer"
-
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
+import Footer from '../Pages/Shared/Footer/Footer';
 const MainLayout = () => {
   return (
-    <div className="max-w-7xl mx-auto">
-      <Navbar/>
-      <Outlet />
-      <Footer/>
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto">
+        <Outlet />
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center">
-      <div>Logo</div>
-      <div>Link</div>
-      <div>Profile</div>
+    <div className="shadow-lg w-full">
+      <div className="flex justify-between items-center p-6 max-w-7xl mx-auto">
+        <div className="text-3xl font-bold ">Logo</div>
+        <div>
+          <ul className="flex gap-4">
+            <Link to='/' className="text-lg font-semibold">Counter</Link>
+            <li className="text-lg font-semibold">Product</li>
+          </ul>
+        </div>
+        <div>Profile</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
