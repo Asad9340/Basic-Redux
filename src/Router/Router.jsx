40a46ca1cc09482eb62569f68d2832d7daeabf69
Asad9340 <asadulimran1999@gmail.com>
2025-06-22 23:55:50 +0600

    added a feedback section

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import Feedback from "../Pages/Feedback/Feedback";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element:<Home/>
+      },
+      {
+        path: '/feedback',
+        element:<Feedback/>
       }
     ]
   },
